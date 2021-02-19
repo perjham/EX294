a0f174de29f4edb1e62558c2a363da4da567b748
PERjham <perjham@gmail.com>
2021-02-18 22:54:09 -0500

    first commit

--- 04-setuphosts.sh
+++ 04-setuphosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ansible -i inventory localhost -m file -a "path=~/.ssh mode='0700' state=directory"
 ansible -i inventory localhost -m openssh_keypair -a "path=~/.ssh/id_rsa"
-# if the user ansible from controller already have ssh powerless access into root user's nodes
+# if the user ansible from controller already have ssh passwordless access into root user's nodes
 # use this way, otherwise you have to add '-k' option for prompt the password in each execution of 
 # the next lines.
 ansible -i inventory all -u root -m user -a "name=ansible"
