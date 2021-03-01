9b304bba1e6a941280d0d2c266d7d58488743395
PERjham <perjham@gmail.com>
2021-03-01 09:47:33 -0500

    Second Attempt

--- 04-setuphosts.sh
+++ 04-setuphosts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-ansible -i inventory localhost -u root -m file -a "path=~/.ssh state=directory mode=0700"
-ansible -i inventory localhost -u root -m openssh_keypair -a "path=~/.ssh/id_rsa"
-ansible -i inventory all -u root -m user -a "name=ansible"
-ansible -i inventory all -u root -m copy -a "content='ansible ALL=(ALL) NOPASSWD: ALL' dest=/etc/sudoers.d/ansible"
-ansible -i inventory all -u root -m authorized_key -a "user=ansible state=present key='{{ lookup('file', '/home/ansible/.ssh/id_rsa.pub') }}'"
+ansible -i inventory localhost -m file -a "path=~/.ssh state=directory owner=ansible group=ansible"
+ansible -i inventory localhost -m openssh_keypair -a "path=~/.ssh/id_rsa"
+ansible -i inventory all -u root -k -m user -a "name=ansible"
+ansible -i inventory all -u root -k -m authorized_key -a "user=ansible state=present key='{{ lookup('file', '~/.ssh/id_rsa.pub') }}'"
+ansible -i inventory all -u root -k -m copy -a "content='ansible ALL=(ALL) NOPASSWD: ALL' dest=/etc/sudoers.d/ansible"
+ansible -i inventory all -m ping
--- 05-configure-repos.sh
+++ 05-configure-repos.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-ansible -i inventory all -b -u ansible -m shell -a "rm -rf /etc/yum.repos.d/*"
-ansible -i inventory all -b -u ansible -m yum_repository -a "name=BaseOS description=BaseOS baseurl=ftp://controller.example.com/BaseOS enabled=yes gpgkey=ftp://192.168.122.100/RPM-GPG-KEY-redhat-release"
-ansible -i inventory all -b -u ansible -m yum_repository -a "name=AppStream description=AppStream baseurl=http://controller.example.com/AppStream enabled=yes gpgkey=http://192.168.122.100/RPM-GPG-KEY-redhat-release"
-cat > hosts.tmp << EOF
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
-::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-192.168.122.100 controller.example.com controller
-192.168.122.101 node1.example.com node1
-192.168.122.102 node2.example.com node2
-192.168.122.103 node3.example.com node3
-192.168.122.104 node4.example.com node4
-EOF
-ansible -i inventory all -b -u ansible -m copy -a "src=./hosts.tmp dest=/etc/hosts owner=root group=root"
+ansible -i inventory all -m shell -u ansible -b -a 'rm -rf /etc/yum.repos.d/*'
+ansible -i inventory all -m copy -u ansible -b -a 'content="127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4\n::1         localhost localhost.localdomain localhost6 localhost6.localdomain6\n192.168.122.100 controller.example.com controller\n192.168.122.101 node1.example.com node1\n192.168.122.102 node2.example.com node2\n192.168.122.103 node3.example.com node3\n192.168.122.104 node4.example.com node4" dest=/etc/hosts'
+ansible -i inventory all -m yum_repository -u ansible -b -a "name=BaseOS description=BaseOS baseurl=ftp://controller.example.com/BaseOS gpgcheck=yes gpgkey=ftp://controller.example.com/RPM-GPG-KEY-redhat-release"
+ansible -i inventory all -m yum_repository -u ansible -b -a "name=AppStream description=AppStream baseurl=http://controller.example.com/AppStream gpgcheck=yes gpgkey=http://controller.example.com/RPM-GPG-KEY-redhat-release"
+ansible -i inventory all -m dnf -u ansible -b -a "name=vim state=latest"
+ansible -i inventory all -m dnf -u ansible -b -a "name=bash-completion state=latest"
